bfc1c9ed01aebcddd56cef1f69ad7bcaee869b5a
Reham <r_nour30@yahoo.com>
2024-02-02 03:43:01 -0800

    up

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -2,4 +2,4 @@
 #Ruby script that accepts one argument and pass it to/
 # regular expression matching method
 # The regular expression must be only matching: capital letters
-puts ARGV[0].scan(/[A-Z/).join
+puts ARGV[0].scan(/[A-Z*/).join
