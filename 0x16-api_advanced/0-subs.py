c3f3109b56558f99be8e9ea9edef581942e0ddf5
Reham <r_nour30@yahoo.com>
2024-06-04 10:31:51 -0700

    up

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent':
+               'python:subreddit.subscriber.count:v1.0 (by /u/reham_saeed)'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
